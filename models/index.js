1527db74c7f4edf7f882815b31ae31207514709c
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-07-31 20:45:03 -0500

    routes for posts table

--- models/index.js
+++ models/index.js
@@ -1,6 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+// A user can have many posts
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
 
+// posts belong to users
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
 module.exports = { User, Post };
